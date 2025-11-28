7c0622093fa202d1f8e7b609128b61cfc392f48e
zerovo-o <70023363+zerovo-o@users.noreply.github.com>
2025-11-28 17:11:34 +0800

    Add files via upload

--- Scenes/Pooler.gd
+++ Scenes/Pooler.gd
@@ -28,33 +28,42 @@ func start_off_object(pool_size: int, object : PackedScene, pool : Array):
 func get_pop():
 	if pop_pool.size() > 0:
 		var obj = pop_pool.pop_front()
-		obj.set_process(true)
-		obj.set_physics_process(true)
-		obj.show()
-		return obj
+		if obj != null and is_instance_valid(obj):
+			obj.set_process(true)
+			obj.set_physics_process(true)
+			obj.show()
+			return obj
+		else:
+			# 如果对象无效，则创建新实例
+			return pop_effect.instantiate()
 	else:
 		return pop_effect.instantiate()
 	
 func return_pop(obj):
-	obj.set_process(false)
-	obj.set_physics_process(false)
-	obj.hide()
-	pop_pool.append(obj)
+	if obj != null and is_instance_valid(obj):
+		obj.set_process(false)
+		obj.set_physics_process(false)
+		obj.hide()
+		pop_pool.append(obj)
 
 func get_gem():
 	if gem_pool.size() > 0:
 		var obj = gem_pool.pop_front()
-		obj.set_process(true)
-		obj.set_physics_process(true)
-		obj.show()
-		return obj
+		if obj != null and is_instance_valid(obj):
+			obj.set_process(true)
+			obj.set_physics_process(true)
+			obj.show()
+			return obj
+		else:
+			# 如果对象无效，则创建新实例
+			return gem.instantiate()
 	else:
 		return gem.instantiate()
 	
 func return_gem(obj):
-	obj.global_position = Vector2(1000,1000)
-	obj.set_process(false)
-	obj.set_physics_process(false)
-	obj.hide()
-	gem_pool.append(obj)
-		
+	if obj != null and is_instance_valid(obj):
+		obj.global_position = Vector2(1000,1000)
+		obj.set_process(false)
+		obj.set_physics_process(false)
+		obj.hide()
+		gem_pool.append(obj)
